09d6f86c76a38f17c0fd0700ca3358a141754dc4
Attila101498 <attila101498@gmail.com>
2025-04-03 12:59:45 +0200

    Set header to UTF-8 in mail.php

--- mail.php
+++ mail.php
@@ -3,16 +3,24 @@
     $name = $_POST['name']; 
     $email_address = $_POST['email']; 
     $message = $_POST['message'];
-    
-    if((empty($name)) || (empty($email_address)) || (empty($message))) {
+
+    if ((empty($name)) || (empty($email_address)) || (empty($message))) {
         $error = true;
     }
 
-    if(!$error) {
+    if (!$error) {
         $to = "somogyim@somogyimasszazs.hu";
         $subject = "Üzeneted érkezett tőle: $name";
-        $headers = "From: $email_address";
 
-        mail($to,$subject,$message,$headers);
+        $headers = "From: $email_address\r\n";
+        $headers .= "Reply-To: $email_address\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+        $headers .= "Content-Transfer-Encoding: 8bit\r\n";
+
+        $subject = "=?UTF-8?B?" . base64_encode($subject) . "?=";
+        $message = mb_convert_encoding($message, "UTF-8", "auto");
+
+        mail($to, $subject, $message, $headers);
     }
 ?>
